456a66be4a267ab8dee940a91bc63fdcae02dcd6
Max Schroeder <max.schroeder@mymail.champlain.edu>
2025-10-23 10:21:27 -0400

    Making basic debug ui

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Agent : MonoBehaviour
+public class Agent : MonoBehaviour
 {
     public List<Vector2> path = new List<Vector2>();
-    public abstract void GeneratePath();
-    public abstract void CheckPath(); //Information Reuse
+    public void GeneratePath()
+    {
+
+    }
+    //Information Reuse
+    public bool CheckPath()
+    {
+        foreach(var point in path)
+        {
+            //if a point is bad, return false
+        }
+        return true;
+    }
 
 
 }
