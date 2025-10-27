d4c73855624dabd1a4785c3d98eee6f3716a19f0
Max Schroeder <max.schroeder@mymail.champlain.edu>
2025-10-26 21:02:27 -0400

    Adding something

--- Assets/Scripts/GridManagerScript.cs
+++ Assets/Scripts/GridManagerScript.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
-using static UnityEngine.EventSystems.EventTrigger;
 
 public class GridManagerScript : MonoBehaviour
 {
